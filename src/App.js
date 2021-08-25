821ff69f431c7d98865af15db1f20b425724e88b
Pinak Patel <pinak.p@elaunchinfotech.in>
2021-08-25 17:34:27 +0530

    Added Component Entries in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
 import { FirstComponent } from './components/FirstComponent';
 import { SecondComponent } from './components/SecondComponent';
+import { ThirdComponent } from './components/ThirdComponent';
+import { FourthComponent } from './components/FourthComponent';
+import { FifthComponent } from './components/FifthComponent';
+import { SixthComponent } from './components/SixthComponent';
+import { SeventhComponent } from './components/SeventhComponent';
+import { EighthComponent } from './components/SeventhComponent';
+import { NinethComponent } from './components/EighthComponent';
+import { TenthComponent } from './components/NinethComponent';
+import { EleventhComponent } from './components/TenthComponent';
 
 
 function App() {
@@ -8,6 +17,15 @@ function App() {
     <div className="App">
       <FirstComponent/>
       <SecondComponent/>
+      <ThirdComponent/>
+      <FourthComponent/>
+      <FifthComponent/>
+      <SixthComponent/>
+      <SeventhComponent/>
+      <EighthComponent/>
+      <NinethComponent/>
+      <TenthComponent/>
+      <EleventhComponent/>
     </div>
   );
 }
